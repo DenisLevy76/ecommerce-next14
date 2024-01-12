f8ffe3ff6edf6d4839bb6f2e3bc8e6d0c718b9b4
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-01-11 21:01:27 -0300

    fix bg color in HighlightedProductCard component

--- src/components/highlighted-product-card/index.tsx
+++ src/components/highlighted-product-card/index.tsx
@@ -30,7 +30,7 @@ export const HighlightedProductCard: React.FC<HighlightedProductCardProps> = ({
       />
       <div
         title={title}
-        className="flex gap-4 text-sm md:text-base md:gap-8 items-center absolute max-w-[65%] bottom-9 left-1/2 md:left-9 -translate-x-1/2 md:translate-x-0 bg-foreground/50 px-2 py-1 rounded-md"
+        className="flex gap-4 text-sm md:text-base md:gap-8 items-center absolute max-w-[65%] bottom-9 left-1/2 md:left-9 -translate-x-1/2 md:translate-x-0 bg-background/50 px-2 py-1 rounded-md"
       >
         <p className="md:text-base font-medium truncate">{title}</p>
         <span className="md:text-base">{formatCurrency(value)}</span>
